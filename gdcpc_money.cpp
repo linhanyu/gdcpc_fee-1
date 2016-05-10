7ed70853a1fd9111d2de2b1e0bd44248a55e66f3
Dobbby <1321755451@qq.com>
2016-05-10 17:58:29 +0800

    binary(pivot)

--- gdcpc_money.cpp
+++ gdcpc_money.cpp
@@ -14,7 +14,7 @@ bool cmp(double a, double b) {
 int main() {
     int t;
     int n;
-    int i, j;
+    int i, j, pivot;
     double left, right;
     double f, tmp, sum, ave;
     vector<double> money;
@@ -31,23 +31,23 @@ int main() {
         }
         sort(money.begin(), money.end(), cmp);
         i = 0;
-        j = n / 2;
+        j = n;
         while (true) {
             left = 0;
-            for (int k = i; k < j; ++k) {
+            pivot = (i + j) / 2;
+            for (int k = 0; k < pivot; ++k) {
                 left += money[k];
             }
             right = sum - left;
-            ave = (left + right - left * f) / (n - f * j);
+            ave = (left + right - left * f) / (n - f * pivot);
 
-            if (money[j] <= ave && money[j - 1] >= ave) {
+            if (money[pivot] <= ave && money[pivot - 1] >= ave) {
                 cout << ave << endl;
                 break;
-            }
-            else if (money[j - 1] < ave) {
-                j = j / 2;
-            } else if (money[j] > ave) {
-                j += (n - j) / 2;
+            } else if (money[pivot - 1] < ave) {
+                j = pivot;
+            } else if (money[pivot] > ave) {
+                i = pivot;
             }
         }
     }
